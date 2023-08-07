25ffc1ae86af2af0a28e1cc03356d4b1643516bc
theba <theba.limbu07@gmail.com>
2023-08-08 00:07:08 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const url = "https://catfact.ninja/";
+const val = {page : 1};
+
+const page = {};
+// cat-content
+page.content = document.querySelector('.cat-content');
+
+function getCats(){
+    const baseUrl = url + "breeds" + "?page=" + val.page;
+    fetch(baseUrl).then(res => res.json())
+    .then(json => {
+        console.log(json);
+        console.log(json.data);
+
+        renderPage(json.data);
+    })
+}
+
+getCats();
+
+
+// render function
+
+function renderPage(data){
+    data.forEach(cat => {
+        const div = document.createElement('div');
+        div.className = 'divCat';
+        const para = document.createElement('p');
+        para.className = 'paraCat';
+        para.textContent = `country: ${cat.country}`
+        div.textContent = `breed: ${cat.breed}`;
+        div.append(para);
+        page.content.append(div);
+    })
+}
